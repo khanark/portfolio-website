d03a7f26094f8f259b2743fc3c212774c9d0a07a
JScodeKiddie <JScodeKiddie@gmail.com>
2023-06-12 18:02:13 +0200

    fix the closing of the dropdown navigation

--- src/app.js
+++ src/app.js
@@ -1,15 +1,38 @@
+// ********** Eleement selectors **********
 const dropDownBtn = document.querySelector('.dropdown-btn');
+const downloadBtn = document.querySelector('.btn');
+const dropDownNav = document.querySelector('.dropdown-nav');
+const header = document.querySelector('.site-header');
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM is loaded');
-
-  dropDownBtn.addEventListener('click', () => {
-    const dropDownNav = document.querySelector('.dropdown-nav');
-    const header = document.querySelector('.site-header');
-
-    dropDownNav.classList.toggle('show');
-    dropDownNav.classList.toggle('dropdown-animation');
-    console.log(dropDownNav);
-    header.classList.toggle('overflow');
+// finish the observer when you have time
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
   });
 });
+
+// ********** event listeners **********
+document.addEventListener('DOMContentLoaded', () => {
+  // open the dropdown menu when you click on the button
+  dropDownBtn.addEventListener('click', onDropDownBtnClick);
+
+  // observer.observe(downloadBtn);
+
+  // close the dropdown menu when you click outside of it
+  document.addEventListener('click', onDocumentClick);
+});
+
+// ********** functions **********
+function onDocumentClick(e) {
+  if (e.target.nodeName !== 'nav' && e.target.nodeName !== 'svg') {
+    dropDownNav.classList.remove('show');
+    dropDownNav.classList.remove('dropdown-animation');
+    header.classList.remove('overflow');
+  }
+}
+
+function onDropDownBtnClick() {
+  dropDownNav.classList.toggle('show');
+  dropDownNav.classList.toggle('dropdown-animation');
+  header.classList.toggle('overflow');
+}
