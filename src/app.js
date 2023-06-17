56a92bdf72819c71c6d7143d9466de6b3b893b08
JScodeKiddie <JScodeKiddie@gmail.com>
2023-06-17 13:11:12 +0200

    create sticky nav

--- src/app.js
+++ src/app.js
@@ -1,57 +1,76 @@
 // ********** element selectors **********
-const header = document.querySelector('.site-header');
-const introduction = document.querySelectorAll('.introduction');
-const projects = document.querySelectorAll('.projects-wrapper');
-const projectSection = document.querySelector('.projects');
-const dropDownBtn = document.querySelector('.dropdown-btn');
-const downloadBtn = document.querySelector('.btn');
-const dropDownNav = document.querySelector('.dropdown-nav');
+const header = document.querySelector('.site-header')
+const introduction = document.querySelectorAll('.introduction')
+const projects = document.querySelectorAll('.projects-wrapper')
+const projectSection = document.querySelector('.projects')
+const dropDownBtn = document.querySelector('.dropdown-btn')
+const downloadBtn = document.querySelector('.btn')
+const dropDownNav = document.querySelector('.dropdown-nav')
 
 // ********** intersection observer **********
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(
     (entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('introduction-animation');
+        entry.target.classList.add('introduction-animation')
       }
     },
     {
       root: document.querySelector('.about'),
       treshold: 0.5,
     }
-  );
-});
+  )
+})
+
+const stickyNavigationObserver = new IntersectionObserver(
+  (entries) => {
+    const techstackSection = entries[0]
+
+    if (techstackSection.isIntersecting) {
+      header.classList.remove('sticky')
+    } else {
+      header.classList.add('sticky')
+    }
+  },
+  {
+    root: null,
+    treshold: 0.5,
+    rootMargin: '-62px',
+  }
+)
 
 // ********** event listeners **********
 document.addEventListener('DOMContentLoaded', () => {
   // open the dropdown menu when you click on the button
-  dropDownBtn.addEventListener('click', onDropDownBtnClick);
+  dropDownBtn.addEventListener('click', onDropDownBtnClick)
 
   // animate the introduction section when it is in the viewport
   introduction.forEach((quality) => {
-    observer.observe(quality);
-  });
+    observer.observe(quality)
+  })
+
+  stickyNavigationObserver.observe(document.querySelector('.techstack'))
 
   // close the dropdown menu when you click outside of it
-  document.addEventListener('click', onDocumentClick);
-});
+  document.addEventListener('click', onDocumentClick)
+})
 
 // ********** functions **********
 function onDocumentClick(e) {
-  console.log(e.target.className);
-  const className = e.target.nodeName;
+  console.log(e.target.className)
+  const className = e.target.nodeName
 
   if (className !== 'svg' && className !== 'BUTTON' && className !== 'nav') {
-    console.log(className);
-    console.log('clicked outside');
-    dropDownNav.classList.remove('show');
-    dropDownNav.classList.remove('dropdown-animation');
-    header.classList.remove('overflow');
+    console.log(className)
+    console.log('clicked outside')
+    dropDownNav.classList.remove('show')
+    dropDownNav.classList.remove('dropdown-animation')
+    header.classList.remove('overflow')
   }
 }
 
 function onDropDownBtnClick() {
-  dropDownNav.classList.toggle('show');
-  dropDownNav.classList.toggle('dropdown-animation');
-  header.classList.toggle('overflow');
+  dropDownNav.classList.toggle('show')
+  dropDownNav.classList.toggle('dropdown-animation')
+  header.classList.toggle('overflow')
 }
